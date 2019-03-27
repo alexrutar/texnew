10e03af8472dbe5f851bc893b6e4462c7740caf9
Alex Rutar <alexrutarhome@gmail.com>
2019-03-27 19:15:46 -0400

    version 1.2 commit

--- texnew/__init__.py
+++ texnew/__init__.py
@@ -7,7 +7,7 @@ Automatic LaTeX template management.
 :license: MIT, see LICENSE for more details.
 """
 
-__version__ = "1.1"
+__version__ = "1.2"
 
 from texnew.test import parse_errors
 from texnew.update import update
