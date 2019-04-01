da57ab610ab1eebadcecb06bd2542e21b076b1f1
Alex Rutar <alexrutarhome@gmail.com>
2019-04-01 17:22:36 -0400

    updated -h

--- texnew/__main__.py
+++ texnew/__main__.py
@@ -18,6 +18,7 @@ Named:
 
 Optional:
   -u                    Update the target template with the given file.
+  --keep-formatting     When updating, preserve the formatting section in the file.
   -v                    Run verbose.
 
 Other:
@@ -44,12 +45,13 @@ def parse():
                                 help='the name of the template to use')
 
     parser.add_argument('-l', "--list", action="store_true", default=False, dest="lst",help="list existing templates and root folder")
-    parser.add_argument('-c', "--check", action="store_true", default=False, dest="lst",help="check for errors in existing templates")
+    parser.add_argument('-c', "--check", action="store_true", default=False, dest="check",help="check for errors in existing templates")
     parser.add_argument('-u', "--update_file", action="store_true", default=False, dest="update_file",help="update_file the specified file with the desired template")
     parser.add_argument("--user", dest="user", default="",help="specify the user file",nargs=1)
+    parser.add_argument("--keep-formatting", dest="transfer", action="store_true", default=False, help="specify the user file",nargs=0)
 
     args = parser.parse_args()
-    return (args.target[0], args.template_type[0], args.update_file, args.user)
+    return (args.target[0], args.template_type[0], args.update_file, args.user, args.transfer)
 
 def main():
     # special use cases:
@@ -64,9 +66,12 @@ def main():
         run_test()
     else:
         # main program branching
-        target, template_type, update_file, user= parse()
+        target, template_type, update_file, user, tr = parse()
+        transfer=['file-specific preamble', 'document start']
+        if tr:
+            transfer.append('formatting')
         if update_file:
-            run_update(target, template_type)
+            run_update(target, template_type, transfer=transfer)
         else:
             if not target.endswith(".tex"):
                 target = target + ".tex"
