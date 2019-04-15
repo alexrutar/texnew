057882e89b8230e3ae5a8f7fe531386ea94818a8
Alex Rutar <alexrutarhome@gmail.com>
2019-04-15 01:51:38 -0400

    added info method

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 test:
 	-trash test/*
 	-python3 -m texnew new test/test.tex asgn
-	-python3 -m texnew update test/test.tex notes
+	-python3 -m texnew update test/test.tex notes -t doctype
 upload:
 	-pandoc --from=markdown --to=rst --output=README.rst short_description.md
 	-/usr/local/bin/python3 setup.py sdist bdist_wheel
--- texnew/parse.py
+++ texnew/parse.py
@@ -37,15 +37,30 @@ def tn(args):
 
 def tn_update(args):
     if args.format:
-        args.transfer.append('formatting')
+        args.transfer.extend(['formatting','doctype'])
     run_update(args.target[0], args.template[0], transfer=args.transfer)
 
 def tn_check(args):
     run_check(args.names,run_all=args.all)
 
 def tn_info(args):
-    print("not implemented yet")
+    # TODO
+    # point to installation location
+    # have listing option
+    msg = {'lst':"Existing templates:\n"+ "\t".join(available_templates()),
+            'dir':'Root directory: {}'.format(RPath.texnew())}
+    d = vars(args)
+    for k in msg.keys():
+        if d[k]:
+            print(msg[k])
 
+
+def parse_errors(args):
+    checklist = {'texnew_exists': (RPath.texnew().exists(), "Missing template information at '{}'".format(RPath.texnew())),
+            'texnew_workspace_exists': (RPath.workspace().exists(), "Missing workspace directory at '{}'".format(RPath.workspace()))}
+    for k,(check,val) in checklist.items():
+        if not check:
+            print("Error: ", val)
 
 def parse():
     """Main argument parser"""
@@ -55,10 +70,13 @@ def parse():
             default=False,
             dest='verbose',
             help='run verbose mode')
+    parser.add_argument('--version',
+            action='version',
+            version='%(prog)s {}'.format(__version__))
     subparsers = parser.add_subparsers(help="test")
 
     # main arguments
-    parser_main = subparsers.add_parser('new', help='create a new latex template')
+    parser_main = subparsers.add_parser('new', help='create a new template')
     parser_main.set_defaults(func=tn)
     parser_main.add_argument('output',
             type=str,
@@ -79,19 +97,19 @@ def parse():
     parser_update.add_argument('template',
             type=str,
             nargs=1,
-            help='the name of the template to update to')
+            help='the name of the template to update with')
     parser_update.add_argument("-f","--keep-formatting",
             dest="format",
             action="store_true",
             default=False,
-            help="preserve formatting options")
+            help="preserve the formatting block")
     parser_update.add_argument("-t","--transfer",
             nargs="*",
             default=['file-specific preamble', 'main document'],
             help="provide a list of blocks to transfer")
 
     # check arguments
-    parser_check = subparsers.add_parser('check', help='check existing templates for errors')
+    parser_check = subparsers.add_parser('check', help='check templates for errors')
     parser_check.set_defaults(func=tn_check)
     parser_check.add_argument('names',
             nargs="*",
@@ -110,12 +128,14 @@ def parse():
             action="store_true",
             default=False,
             dest="lst",
-            help="list existing templates and root folder")
+            help="list existing templates")
+    parser_info.add_argument('-d', "--directory",
+            action="store_true",
+            default=False,
+            dest="dir",
+            help="display path to the root folder")
 
     args = parser.parse_args()
-    checklist = {'texnew_exists': (RPath.texnew().exists(), "Missing template information at '{}'".format(RPath.texnew()))}
-    for k,(check,val) in checklist.items():
-        if not check:
-            print(val)
+    parse_errors(args)
 
     args.func(args)
